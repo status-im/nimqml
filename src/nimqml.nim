f16dfb30a77cc3a2d5a339927dff23e7b114c7f3
Sale Djenic <aleksandardjenic@status.im>
2021-08-23 09:55:12 +0200

    fix(@desktop/general): clicking push notification does expand the app but does not open correct channel/chat
    An interface for using methods of the StatusOSNotification class added
    in the `dotherside` project.
    
    This is corresponding nimqml part of the issue 2996.
    
    Fixes: #2996

--- src/nimqml.nim
+++ src/nimqml.nim
@@ -41,6 +41,7 @@ include "nimqml/private/qresource.nim"
 include "nimqml/private/qdeclarative.nim"
 include "nimqml/private/singleinstance.nim"
 include "nimqml/private/status/statusevent.nim"
+include "nimqml/private/status/statusosnotification.nim"
 
 
 proc signal_handler*(receiver: pointer, signal: cstring, slot: cstring) =
